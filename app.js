1de078c97ca043fadde46adecc5ca74ddc4f4849
cicatriz-ftw <coldfuzion@gmail.com>
2016-06-08 13:54:30 -0700

    base project in place beginning crud ops

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const helmet  = require('helmet');
+const   express       = require('express'),
+        bodyParser    = require('body-parser'),
+        http          = require('http'),
+        path          = require('path'),
+        helmet        = require('helmet'),
+        aws           = require('aws-sdk'),
+        mongoose      = require('mongoose'),
+        app           = express(),
+        port          = process.env.PORT || 9000,
+        router        = express.Router();
 
-const app = express();
-const port = process.env.PORT || 9000;
 
-
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'jade');
 app.use(helmet());
+app.use('/api', router);
+
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET; 
 
 app.get('/', (req, res) => {
-  res.render('index', { foo:'index' });
+    res.render('account');
+  //res.render('index', { foo:'index' });
 });
 
-app.get('/:id', (req, res) => {
-  const id = req.params.id;
-  res.render('index', { foo:id });
+router.get('/', function(req, res) {
+  res.json({ message: 'Server is go!' });
 });
+
+mongoose.connect('mongodb://localhost:27017/models');
 
 app.listen(port, '', err => {
   if(err) return console.log(err);
   console.log(`Listening at http://localhost:${port}`);
+});
+
+app.get('/sign_s3', (req, res) => {
+    aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+    var s3 = new aws.S3();
+    var s3_params = {
+        Bucket: S3_BUCKET,
+        Key: req.query.file_name,
+        Expires: 60,
+        ContentType: req.query.file_type,
+        ACL: 'public-read'
+    };
+    s3.getSignedUrl('putObject', s3_params, (err, data) => {
+        if(err){
+            console.log(err);
+        }else{
+            var return_data = {
+                signed_request: data,
+                url: 'https://'+S3_BUCKET+'.s3.amazonaws.com/'+req.query.file_name
+            };
+            res.write(JSON.stringify(return_data));
+            res.end();
+        }
+    });
+});
+
+app.post('/submit_form', (req, res) => {
+    username = req.body.username;
+    full_name = req.body.full_name;
+    avatar_url = req.body.avatar_url;
+    update_account(username, full_name, avatar_url); // TODO: create this function
+    // TODO: Return something useful or redirect
 });
--- public/_js/script.js
+++ public/_js/script.js
@@ -1,6 +1,46 @@
+
+function getSignedRequest(file){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/sign_s3?file_name='+file.name+'$&file_type='+file.type);
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            if(xhr.status ===200){
+                var response = JSON.parse(xhr.responseText);
+                upload_file(file, response.signed_request, response.url);
+            }else{
+                alert('Could not get signed URL.');
+            }
+        }
+    };
+    xhr.send();
+}
+
+function upload_file(file, signed_request, url){
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', signed_request);
+    xhr.setRequestHeader('x-amz-acl', 'public-read');
+    xhr.onload = function(){
+        if (xhr.status === 200){
+            document.getElementById('preview').src = url;
+            document.getElementById('avatar_url').value = url;
+        }
+    };
+    xhr.onerror = function() {
+        alert('Could not upload file.');
+    };
+    xhr.send(file);
+}
+
 function init(){
-		//LETS DO THIS!
-    console.log('test');
+    document.getElementById('file_input').onchange = function(){
+        var files = this.files;
+        var files = files[0];
+        if (file == null){
+            alert('No file selected');
+        }else{
+            get_signed_request(file);
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', init, 0);
